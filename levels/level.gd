154acc4a644f07bdc1fcf4f1dd472c08c564afcb
Vasil <vasiltopalovic@outlook.com>
2024-04-09 22:16:33 -0400

    feat: add leaderboard

--- levels/level.gd
+++ levels/level.gd
@@ -7,13 +7,28 @@ extends Node3D
 
 var track1 = preload("res://sound/track1.wav")
 
+var url = "http://192.168.2.16:8000/1/leaderboard"
 var timer = 0
 var completed = false
 var started = false
 
+var leaderboard = ""
+
 func _ready():
 	start_zone.get_node("Area3D").body_exited.connect(player_started)
 	end_zone.get_node("Area3D").body_entered.connect(player_finished)
+	var name = get_tree().current_scene.name
+	url = "http://192.168.2.16:8000/" + name + "/leaderboard"
+	
+	$GetLeaderboard.request_completed.connect(handle_request)
+	$GetLeaderboard.request(url)
+	
+func handle_request(result, response_code, headers, body):
+	var json = JSON.parse_string(body.get_string_from_utf8())
+	
+	for run in json:
+		
+		leaderboard += run.username + " | " + str(snapped(run.time_ms / 1000, 0.01)) + "\n"
 
 func player_started(col):
 	
@@ -23,12 +38,28 @@ func player_started(col):
 	started = true
 
 func player_finished(col):
+	
+	
+	if not completed:
+		var body = JSON.stringify({
+			"time_ms": floor(timer * 1000)
+		})
+		var headers = ["Content-Type: application/json", "user_id: " + User.uuid]
+		$PostLeaderboard.request(url, headers, HTTPClient.METHOD_POST, body)
+	
+	
 	completed = true
 	
 func _process(delta):
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_pressed("jump"):
 		player_started({})
 		
 	if not completed and started:
 		timer += delta
 		timer_label.text = str(snapped(timer, 0.01)) + " s"
+		
+	if Input.is_action_pressed("leaderboard"):
+		$Leaderboard.text = leaderboard
+	else:
+		$Leaderboard.text = ""
+		
--- menus/level_select/level_select.gd
+++ menus/level_select/level_select.gd
@@ -2,17 +2,67 @@ extends Control
 
 @onready var lj = $longjump
 @onready var bh = $bhop
+@onready var bh2 = $bhop2
+
+var url = "http://192.168.2.16:8000/user"
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	lj.pressed.connect(longjump)
 	bh.pressed.connect(bhop)
+	bh2.pressed.connect(bhop2)
+	
+	var save_file = FileAccess.open('user://source.data', FileAccess.READ)
+	
+	if save_file == null:
+		print(FileAccess.get_open_error())
+		$username.visible = true
+		$logout.visible = false
+		$submit_username.visible = true
+		$submit_username.pressed.connect(create_user)
+		$CreateUser.request_completed.connect(save_uuid)
+	else:
+		$logout.pressed.connect(logout)
+		var uuid = save_file.get_as_text()
+		User.uuid = uuid
 
+func logout():
+	DirAccess.remove_absolute('user://source.data')
+	$username.visible = true
+	$logout.visible = false
+	$submit_username.visible = true
+	$submit_username.pressed.connect(create_user)
+	$CreateUser.request_completed.connect(save_uuid)
+	
+func save_uuid(result, response_code, headers, body):
+	print(response_code)
+	if response_code != 200:
+		return
+		
+	var save_file = FileAccess.open('user://source.data', FileAccess.WRITE_READ)
+	var json = JSON.parse_string(body.get_string_from_utf8())
+	print(json)
+	save_file.store_string(json.id)
+	User.uuid = json.id
+	$username.visible = false
+	$submit_username.visible = false
+
+func create_user():
+	var body = JSON.stringify({
+			"username": $username.text,
+	})
+	print(body)
+	var headers = ["Content-Type: application/json"]
+	$CreateUser.request(url, headers, HTTPClient.METHOD_POST, body)
+	
 func longjump():
 	get_tree().change_scene_to_file("res://longjump.tscn")
 	
 func bhop():
 	get_tree().change_scene_to_file("res://levels/1.tscn")
+	
+func bhop2():
+	get_tree().change_scene_to_file("res://levels/2.tscn")
 	
 func _process(delta):
 	pass
