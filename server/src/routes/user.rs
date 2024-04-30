3f7949c0a7777cb32f71ba8c4a87fef4e2f4c11c
vasiltop <vasiltopalovic@outlook.com>
2024-04-30 13:29:23 -0400

    feat: login route

--- server/src/routes/user.rs
+++ server/src/routes/user.rs
@@ -22,8 +22,31 @@ pub fn router() -> Router<AppState> {
 		.route("/register", post(register))
 }
 
-async fn login(State(pool): State<AppState>, Json(user): Json<User>) -> String {
-	user.username
+async fn login(
+	State(pool): State<AppState>,
+	Json(input): Json<User>,
+) -> Result<String, ResponseError> {
+	input.validate()?;
+
+	let user = sqlx::query!(
+		r#"SELECT * FROM "user" WHERE username = $1"#,
+		input.username
+	)
+	.fetch_one(&pool)
+	.await
+	.map_err(|_| ResponseError::QueryError)?;
+
+	let valid = bcrypt::verify(
+		input.password,
+		std::str::from_utf8(&user.password).map_err(|_| ResponseError::QueryError)?,
+	)
+	.map_err(|_| ResponseError::QueryError)?;
+
+	if valid {
+		Ok(user.id.to_string())
+	} else {
+		Err(ResponseError::QueryError)
+	}
 }
 
 #[axum::debug_handler]
@@ -48,16 +71,14 @@ async fn register(
 	let hashed = bcrypt::hash(input.password, bcrypt::DEFAULT_COST)
 		.map_err(|_| ResponseError::QueryError)?;
 
-	let query = sqlx::query!(
-		r#"INSERT INTO "user" (username, password, admin) VALUES ($1, $2, FALSE)"#,
+	let user = sqlx::query!(
+		r#"INSERT INTO "user" (username, password, admin) VALUES ($1, $2, FALSE) RETURNING id"#,
 		input.username,
 		hashed.as_bytes()
 	)
-	.execute(&pool)
+	.fetch_one(&pool)
 	.await
 	.map_err(|_| ResponseError::QueryError)?;
 
-	println!("{:?}", user);
-
-	Ok(input.username)
+	Ok(user.id.to_string())
 }
