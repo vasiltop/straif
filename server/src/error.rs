08f25145a64f38bfbf785b05c62b44534a02395f
vasiltop <vasiltopalovic@outlook.com>
2024-05-01 10:43:21 -0400

    feat: add bhop leaderboard routes

--- server/src/error.rs
+++ server/src/error.rs
@@ -6,13 +6,16 @@ pub enum Error {
 	ValidationError(#[from] validator::ValidationErrors),
 	#[error("auth error: {0}")]
 	Auth(#[from] crate::routes::user::Error),
+	#[error("bhop error: {0}")]
+	Bhop(#[from] crate::routes::bhop::Error),
 }
 
 impl IntoResponse for Error {
 	fn into_response(self) -> Response<Body> {
 		let (status, error) = match self {
-			Self::ValidationError(e) => (StatusCode::BAD_REQUEST, e.to_string().into_response()),
-			Self::Auth(e) => (e.status(), e.to_string().into_response()),
+			Self::ValidationError(e) => (StatusCode::BAD_REQUEST, e.to_string()),
+			Self::Auth(e) => (e.status(), e.to_string()),
+			Self::Bhop(e) => (StatusCode::REQUEST_TIMEOUT, e.to_string()),
 		};
 
 		(status, error).into_response()
--- server/src/routes/bhop.rs
+++ server/src/routes/bhop.rs
@@ -1,16 +1,86 @@
-use axum::{extract::Json, routing::post, Router};
-use serde::Deserialize;
-
 use crate::AppState;
+use axum::{
+	extract::{Json, State},
+	response::IntoResponse,
+	routing::{get, post},
+	Router,
+};
+use serde::Deserialize;
+use serde::Serialize;
+use sqlx::types::Uuid;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+	#[error("invalid map name")]
+	InvalidMapName,
+	#[error("invalid submission")]
+	InvalidSubmission,
+}
+
+#[derive(Serialize, Deserialize)]
+struct RunOutput {
+	name: String,
+	run: Option<Vec<u8>>,
+}
 
 #[derive(Deserialize)]
-struct Test {
-	foo: String,
-}
-pub fn router() -> Router<AppState> {
-	Router::new().route("/", post(test))
+struct RunInput {
+	name: String,
+	id: Uuid,
+	run: Vec<u8>,
 }
 
-async fn test(Json(payload): Json<Test>) -> String {
-	payload.foo
+#[derive(Deserialize)]
+struct Map {
+	name: String,
+}
+
+struct MapId {
+	id: Uuid,
+}
+
+pub fn router() -> Router<AppState> {
+	Router::new()
+		.route("/publish", post(publish))
+		.route("/leaderboard", get(leaderboard))
+}
+
+async fn publish(
+	State(pool): State<AppState>,
+	Json(run): Json<RunInput>,
+) -> Result<(), crate::error::Error> {
+	print!("hit");
+	let map_id = sqlx::query_as!(MapId, "SELECT id FROM map WHERE name = $1", run.name)
+		.fetch_one(&pool)
+		.await
+		.map_err(|_| Error::InvalidMapName)?;
+
+	sqlx::query!(
+		"INSERT INTO placement_bhop VALUES ($1, $2, $3, $4) ON CONFLICT (user_id, map_id) DO UPDATE SET time_ms = $4 WHERE placement_bhop.time_ms > $4 ",
+		run.id,
+		map_id.id,
+		run.run,
+		9
+		//TODO: Make the run parser to calculate time
+	)
+	.execute(&pool)
+	.await
+	.map_err(|_| Error::InvalidSubmission)?;
+
+	Ok(())
+}
+
+async fn leaderboard(
+	State(pool): State<AppState>,
+	Json(map): Json<Map>,
+) -> Result<impl IntoResponse, crate::error::Error> {
+	let runs = sqlx::query_as!(
+		RunOutput,
+		"SELECT name, run FROM bhop_leaderboard INNER JOIN map ON bhop_leaderboard.map_id = map.id WHERE map.name = $1 LIMIT 10",
+		map.name
+	)
+	.fetch_all(&pool)
+	.await.map_err(|_| Error::InvalidMapName)?;
+
+	Ok(Json(runs))
 }
