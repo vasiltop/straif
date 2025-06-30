7768519ffd425e2dcb8a549279c9cacecf7016e1
Vasil Topalovic <vasiltopalovic@outlook.com>
2025-06-30 01:59:49 -0400

    feat: switch app id

--- src/main.gd
+++ src/main.gd
@@ -1,7 +1,9 @@
 extends Node
 
+const APP_ID := 3850480
+
 func _ready() -> void:
-	var init_res := Steam.steamInitEx(480, true)
+	var init_res := Steam.steamInitEx(APP_ID, true)
 
 	if init_res.status != Steam.SteamAPIInitResult.STEAM_API_INIT_RESULT_OK:
 		print("Failed to initialize, Steam: %s" % init_res)
