5497483cae90d490816dbe3c81a0a1a5db30cfbf
Vasil Topalovic <vasiltopalovic@outlook.com>
2025-07-04 00:31:17 -0400

    feat: make targets more visible

--- src/target/target.gd
+++ src/target/target.gd
@@ -6,3 +6,11 @@ var health: float = 100
 
 func _ready() -> void:
 	tree_exited.connect(map.target_killed.emit)
+
+func _process(_delta: float) -> void:
+	var target_position :=  map.player.global_position
+	var current_position := global_position
+
+	target_position.y = current_position.y
+
+	look_at(target_position, Vector3.UP)
