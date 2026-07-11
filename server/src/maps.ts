5d3e95e0bd8200d92aebb7f5bfb2afd33aea3fb5
Vasil Topalovic <vasiltop@proton.me>
2026-07-11 14:17:07 -0400

    fix: prod crash

--- server/src/maps.ts
+++ server/src/maps.ts
@@ -1,10 +1,5 @@
-import { readFileSync } from 'node:fs';
-import { join } from 'node:path';
 import { run_mode } from './db/schema';
-
-const map_data = JSON.parse(
-  readFileSync(join(process.cwd(), '../maps.json'), 'utf-8')
-);
+import mapData from '../../maps.json';
 
 export type RunMode = (typeof run_mode.enumValues)[number];
 
@@ -12,12 +7,12 @@ export type Map = {
   name: string;
   tier: number;
   modes: string[];
-  medals_target: string[] | undefined;
-  medals_bhop: string[] | undefined;
+  medals_target?: number[];
+  medals_bhop?: number[];
 };
 
 export function get_maps(): Map[] {
-  return map_data.maps;
+  return mapData.maps;
 }
 
 export function get_maps_of_mode(mode: RunMode): Map[] {
